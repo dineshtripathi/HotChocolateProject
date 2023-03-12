39b26dc4cd22e65dc3866c3ac49b22545ae89525
Dinesh Tripathi <hitechdinesh@gmail.com>
2023-03-12 19:14:43 +0000

    added references

--- Hotchocolate.BankAccount/PropertyAccount.Api/PropertyAccount.Api/Program.cs
+++ Hotchocolate.BankAccount/PropertyAccount.Api/PropertyAccount.Api/Program.cs
@@ -1,6 +1,12 @@
+using PropertyAccount.Api.ServiceCollectionExtension;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHotchocolateServiceCollection(builder.Configuration);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL("");
 
 app.Run();
---
+++ Hotchocolate.BankAccount/PropertyAccount.Api/PropertyAccount.Api/ServiceCollectionExtension/HotchocolateServiceExtension.cs
@@ -0,0 +1,30 @@
+﻿using HotChocolate.Types.Pagination;
+
+namespace PropertyAccount.Api.ServiceCollectionExtension;
+
+public static class HotchocolateServiceExtension
+{
+    public static IServiceCollection AddHotchocolateServiceCollection(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddGraphQLServer().AddQueryType(q => q.Name("Query"))
+            .SetPagingOptions(new PagingOptions
+            {
+                IncludeTotalCount = true,
+                LegacySupport = true,
+                DefaultPageSize = 100,
+                MaxPageSize = 100,
+                AllowBackwardPagination = true,
+                InferConnectionNameFromField = true,
+                InferCollectionSegmentNameFromField = true,
+
+            })
+            .AddProjections()
+            .AddFiltering()
+            .AddSorting();
+
+        return services;
+
+
+    }
+}
